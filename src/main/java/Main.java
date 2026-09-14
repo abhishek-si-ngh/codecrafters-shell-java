9332393ec0872e1d212d55654425d5d7f8310b06
Abhishek Singh <asdec0302@gmail.com>
2026-09-14 15:27:14 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
         System.out.print("$ ");
+        Scanner sc=new Scanner(Syetm.in);
+        String command=sc.nextLine();
+        System.out.println(command+": command not found");
+        
     }
 }
