71883a44c248816abf8faa7c63f8d38fd816d296
Abhishek Singh <asdec0302@gmail.com>
2026-09-14 16:21:09 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,6 +22,8 @@ public class Main {
                     System.out.println(command.substring(5)+" is a shell builtin");
                 else if(command.substring(5).equals("echo"))
                     System.out.println(command.substring(5)+" is a shell builtin");
+                else if(command.substring(5).equals("type"))
+                    System.out.println(command.substring(5)+"is a shell builtin");
                 else
                     System.out.println(command.substring(5)+": command not found");         
             }
