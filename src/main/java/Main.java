48b5c9f86c91fbf422ba1937e0d976414e67b9f8
Abhishek Singh <asdec0302@gmail.com>
2026-09-14 16:08:43 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,6 +16,15 @@ public class Main {
             {
                 System.out.println(command.substring(5));
             }
+            else if(command.startsWith("type "))
+            {
+                if(command.substring(5).equals("exit"))
+                    System.out.println(command.substring(5)+" is a shell builtin");
+                else if(command.substring(5).equals("echo"))
+                    System.out.println(command.substring(5)+" is a shell builtin");
+                else
+                    System.out.println(command.substring(5)+": command not found");         
+            }
             else
             System.out.println(command+": command not found");
         }
