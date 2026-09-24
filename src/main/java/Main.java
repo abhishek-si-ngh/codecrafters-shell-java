f06ae21676235c79c8cae6545a87d6706324a05a
Abhishek Singh <asdec0302@gmail.com>
2026-09-24 23:28:29 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -202,9 +202,16 @@ public class Main
                 {
                     //reader.printAbove(String.join("  ",matches));
                     tabCount=0;
-                    reader.callWidget(LineReader.LIST_CHOICES);
+
+                    reader.callWidget(LineReader.CLEAR);
+
+                    reader.getTerminal().writer().println(String.join(" ",matches));
+                    //reader.callWidget(LineReader.LIST_CHOICES);
                     reader.callWidget(LineReader.REDRAW_LINE);
                     reader.callWidget(LineReader.REDISPLAY);
+
+                    reader.getTerminal().write().flush();
+                    
                     return true;
                 }
             }
