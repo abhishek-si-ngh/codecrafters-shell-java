996ae67b6775899b3fce102b8e7f972b856c497a
Abhishek Singh <asdec0302@gmail.com>
2026-09-24 21:28:43 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -21,7 +21,7 @@ public class Main
 
         Terminal terminal=TerminalBuilder.builder().build();
 
-        LineReader reader=LineReaderBuilder.builder().terminal(terminal).completer(new BuiltinCompleter()).option(LineReader.Option.DISABLE_EVENT_EXPANSION,true).option(LineReader.Option.AUTO_LIST,true).option(LineReader.Option.LIST_AMBIGUOUS,true).build();
+        LineReader reader=LineReaderBuilder.builder().terminal(terminal).completer(new BuiltinCompleter()).option(LineReader.Option.DISABLE_EVENT_EXPANSION,true).option(LineReader.Option.AUTO_LIST,false).option(LineReader.Option.LIST_AMBIGUOUS,true).build();
         
         while(true)
         {
